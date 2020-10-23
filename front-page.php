a651d30f443b4c05109017a10ec730f192f34ae9
Dan Eckels <dan@leibowitzdesign.com>
2020-10-23 12:37:44 -0400

    Add graphic

--- front-page.php
+++ front-page.php
@@ -3,8 +3,17 @@ get_header();
 ?>
 
 	<main id="primary" class="home site-main">
-
-	</main><!-- #main -->
+		<section class="hero">
+			<div class="inner">
+				<div class="segment segment__1"></div>
+				<div class="segment segment__2"></div>
+				<div class="segment segment__3"></div>
+				<div class="segment segment__4"></div>
+				<div class="segment segment__5"></div>
+				<div class="segment segment__6"></div>
+			</div>
+		</section>
+	</main>
 
 <?php
-get_footer();
+// get_footer();
